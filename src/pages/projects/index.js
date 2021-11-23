757d12a59c759a853a22667e803d464bc2651d14
Marcelo de León <mdeleoncosentino@gmail.com>
2021-11-22 22:02:26 -0300

    feat: Add thumbnails

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 import { Link, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const Projects = ({ data }) => {
   console.log(data)
@@ -16,6 +17,7 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <GatsbyImage image={getImage(project.frontmatter.thumbnail)} alt="Thumbnail" />
                 <h3>{ project.frontmatter.title }</h3>
                 <p>{ project.frontmatter.headline }</p>
               </div>
@@ -37,6 +39,13 @@ export const query = graphql`
           slug
           headline
           title
+          thumbnail {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+              )
+            }
+          }
         }
         id
       }
--- src/projects/data_processing.md
+++ src/projects/data_processing.md
@@ -4,5 +4,13 @@ headline: Datos faltantes, normalización y outliers.
 slug: data-preprocessing
 ---
 
-# Procesamiento previo de los datos
+Gran parte de la tarea y el esfuerzo del desarrollo de modelos de Machine
+Learning (aproximadamente un 80%) pasa por el procesamiento previo de los
+datos. Es muy importante entender el contexto de los datos, así también como
+identificar si hay faltante de información, outliers o hay necesidad de
+normalizar los datos.
 
+# Manejo de datos faltantes
+![Test](./test.png)
+
+# Outliers
